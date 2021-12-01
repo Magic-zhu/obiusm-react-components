9ba2e657ead75fb010051944f1095e7d10db4462
magic-zhu <18817313320@163.com>
2021-12-01 23:27:21 +0800

    init

---
+++ src/Skeleton/index.md
@@ -0,0 +1,22 @@
+---
+nav:
+  title: Components
+  path: /components
+---
+
+## Skeleton
+
+Demo:
+
+```tsx
+import React from 'react';
+import { Skeleton } from '@obiusm/react-components';
+
+export default () => (
+  <Skeleton isVisible={true}>
+    <div style={{ width: '100px', height: '100px' }}></div>
+  </Skeleton>
+);
+```
+
+More skills for writing demo: https://d.umijs.org/guide/basic#write-component-demo
---
+++ src/Skeleton/index.tsx
@@ -0,0 +1,65 @@
+import React, { ReactElement } from 'react';
+import './index.less';
+interface Props {
+  isVisible: boolean;
+  depth?: number;
+  children: any;
+}
+const createModal = (child: ReactElement, depth: number, current: number) => {
+  if (
+    depth === current ||
+    (child.props && child.props['data-skeletonIgnore'])
+  ) {
+    return;
+  }
+  if (
+    child.props &&
+    child.props.children &&
+    Array.isArray(child.props.children) &&
+    current < depth - 1
+  ) {
+    return (
+      <div
+        className={`${
+          child.props.className !== undefined ? child.props.className : ''
+        } ${'react-skeleton'}`}
+        style={
+          child.props && child.props['data-skeletonStyle']
+            ? child.props['data-skeletonStyle']
+            : {}
+        }
+        key={Math.random() * 1000}
+      >
+        {child.props.children && child.props.children.length > 0
+          ? child.props.children.map((child: any) => {
+              return createModal(child, depth, current + 1);
+            })
+          : '*'}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`${
+          child.props && child.props.className ? child.props.className : ''
+        } ${'react-skeleton2'}`}
+        style={
+          child.props && child.props['data-skeletonStyle']
+            ? child.props['data-skeletonStyle']
+            : {}
+        }
+        key={Math.random() * 1000}
+      >
+        *
+      </div>
+    );
+  }
+};
+function Skeleton(props: Props) {
+  if (props.isVisible) {
+    return createModal(props.children, props.depth || 4, 0);
+  } else {
+    return props.children ? props.children : <div />;
+  }
+}
+export default Skeleton;
