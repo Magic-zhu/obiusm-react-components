2d938864a84c3bce56055d503c0e9c27e83ac3ac
magic-zhu <18817313320@163.com>
2021-12-02 21:15:08 +0800

    feat: update

--- .umirc.ts
+++ .umirc.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'obiusm-react-components',
-  basePth: '/',
-  favicon: './logo.png',
-  logo: './logo.png',
+  base: '/obiusm-react-components/',
+  publicPath: '/obiusm-react-components/',
+  exportStatic: {},
+  favicon: '/obiusm-react-components/logo.png',
+  logo: '/obiusm-react-components/logo.png',
   outputPath: 'docs',
   mode: 'site',
+  dynamicImport: {},
   // more config: https://d.umijs.org/config
 });
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # obiusm-react-components
+
+React common components
+
+- :white_check_mark: Skeleton
+- :white_check_mark: Toast
+
+Docs:
